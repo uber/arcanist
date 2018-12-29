5a3e53d262f4be80f0bae682ffd8f7f1deb78a6c
Stanley Shyiko <shyiko@uber.com>
2018-12-28 17:28:27 -0800

    Add "exec" linter

---
+++ src/lint/linter/ArcanistExecLinter.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ * Execute arbitrary command once per configuration
+ * (regardless of how many files are "include"d).
+ *
+ * `.arclint` example:
+ *
+ *   {
+ *     "linters": {
+ *       "makelint": {
+ *         "type": "exec",
+ *         "exec.command": "make lint",
+ *         "include": "(\\.go$)",
+ *         "exclude": [
+ *           "(^vendor/)"
+ *         ]
+ *       }
+ *     }
+ *   }
+ *
+ * The command will be invoked from the project root, so you can specify a
+ * relative path like `scripts/lint.sh` or an absolute path like
+ * `/opt/lint/lint.sh`.
+ *
+ * The return code of the command must be 0, or an exception will be raised
+ * reporting that the linter failed.
+ */
+final class ArcanistExecLinter extends ArcanistLinter {
+
+  private $command = null;
+
+  public function willLintPaths(array $paths) {
+    $root = $this->getProjectRoot();
+    $future = new ExecFuture('%C', $this->command);
+    $future->setCWD($root);
+    $future->resolvex(); // non-zero exit code will result in an error
+  }
+
+  public function getInfoDescription() {
+    return pht('Execute arbitrary command once per configuration.');
+  }
+
+  public function getLinterName() {
+    return $this->getLinterConfigurationName();
+  }
+
+  public function getLinterConfigurationName() {
+    return 'exec';
+  }
+
+  public function getLinterConfigurationOptions() {
+    $options = array(
+      'exec.command' => array(
+        'type' => 'string',
+        'help' => pht('Command to execute.'),
+      )
+    );
+    return $options + parent::getLinterConfigurationOptions();
+  }
+
+  public function setLinterConfigurationValue($key, $value) {
+    switch ($key) {
+      case 'exec.command':
+        $this->command = $value;
+        return;
+    }
+    return parent::setLinterConfigurationValue($key, $value);
+  }
+
+}
