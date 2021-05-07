25d1fc3fd798aa51aed9431e485554e330bebaa7
Arturas Moskvinas <arturas@uber.com>
2021-05-07 16:50:21 +0300

    Open create jira issue in browser to keep similar to old behavior

--- src/uber/UberTask.php
+++ src/uber/UberTask.php
@@ -244,9 +244,11 @@ final class UberTask extends Phobject {
                   'description' => $description,
                 ));
             $issues[] = $jira_issue['key'];
+            $issue_url = sprintf(self::ISSUE_URL, $jira_issue['key']);
             $this->console
               ->writeOut(pht("Jira issue %s created\n",
-                  sprintf(self::ISSUE_URL, $jira_issue['key'])));
+                  $issue_url));
+            $this->openURIsInBrowser(array($issue_url));
             break;
           }
         }
