262b6572fc6c07877238ee36da8659ce418ba77e
Arturas Moskvinas <arturas@uber.com>
2021-05-07 17:09:28 +0300

    REmvoe line split

--- src/uber/UberTask.php
+++ src/uber/UberTask.php
@@ -246,8 +246,7 @@ final class UberTask extends Phobject {
             $issues[] = $jira_issue['key'];
             $issue_url = sprintf(self::ISSUE_URL, $jira_issue['key']);
             $this->console
-              ->writeOut(pht("Jira issue %s created\n",
-                  $issue_url));
+              ->writeOut(pht("Jira issue %s created\n", $issue_url));
             $this->openURIsInBrowser(array($issue_url));
             break;
           }
