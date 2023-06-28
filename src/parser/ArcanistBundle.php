362afb5528c79200af2593e56e04903eafe1089c
Preetam Dwivedi <preetam@uber.com>
2023-06-27 19:23:38 -0700

    php-8.2-upgrade

--- src/parser/ArcanistBundle.php
+++ src/parser/ArcanistBundle.php
@@ -639,8 +639,7 @@ final class ArcanistBundle extends Phobject {
     $old_path = $change->getOldPath();
     $type = $change->getType();
 
-    if (!strlen($old_path) ||
-        $type == ArcanistDiffChangeType::TYPE_ADD) {
+    if ($old_path === '' || $type == ArcanistDiffChangeType::TYPE_ADD) {
       $old_path = null;
     }
 
