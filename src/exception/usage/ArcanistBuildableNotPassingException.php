227df689c7c93615550a3f9dbd5319c675e8145e
Albert Wu <wua@uber.com>
2024-12-17 09:35:14 -0800

    improve the error message when buildable is not passing

--- src/exception/usage/ArcanistBuildableNotPassingException.php
+++ src/exception/usage/ArcanistBuildableNotPassingException.php
@@ -6,7 +6,7 @@
 final class ArcanistBuildableNotPassingException extends ArcanistUsageException {
 
   public function __construct() {
-    parent::__construct(pht('The Harbormaster Buildable is not passing. Use BREAKGLASS in case of emergency.'));
+    parent::__construct(pht('The Harbormaster Buildable is not passing. Land is not permitted unless all blocking CI jobs pass. If you are running custom CI jobs that are intended to be non-blocking, you can mark the build as non-blocking in its build plan. See t.uber.com/code-land-policy for more information. For emergencies, use BREAKGLASS by adding it to your revision summary. BREAKGLASS will trigger an audit and is reported monthly to EngLT.'));
   }
 
 }
